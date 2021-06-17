362052792e93bb21a97f991966ba1f62f107f623
hyoogu <idhyo0o@gmail.com>
2021-06-18 00:04:29 +0900

    add info

--- data-structure/binaryHeap.js
+++ data-structure/binaryHeap.js
@@ -1,3 +1,24 @@
+// 문제
+// 정수를 요소로 갖는 배열을 입력받아 오름차순으로 정렬하여 리턴해야 합니다.
+
+// 입력
+// 인자 1 : arr
+// number 타입을 요소로 갖는 배열
+// arr[i]는 -100,000 이상 100,000 이하의 정수
+// arr.length는 100,000 이하
+// 출력
+// number 타입을 요소로 갖는 배열을 리턴해야 합니다.
+// 주의사항
+// 힙 정렬을 구현해야 합니다.
+// arr.sort 사용은 금지됩니다.
+// 입력으로 주어진 배열은 중첩되지 않은 1차원 배열입니다.
+// 최소 힙(min heap)을 구현해야 합니다.
+// 최소 힙 구현을 위해 선언된 함수들(getParentIdx, insert, removeRoot)을 전부 완성해야 합니다.
+// swap, getParentIdx, insert, removeRoot를 전부 사용해야 합니다.
+// swap, binaryHeap을 수정하지 않아야 합니다.
+// 테스트 케이스에서 힙 함수들을 정확히 구현했는지 함께 테스트합니다.
+// removeRoot의 시간 복잡도는 O(logN)입니다.
+
 function swap(idx1, idx2, arr) {
   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 }
