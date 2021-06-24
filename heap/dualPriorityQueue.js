d8fbec712e234bae1fba7bf9fcc3ec53d8c10268
hyoogu <idhyo0o@gmail.com>
2021-06-25 01:30:16 +0900

    solve dualPriorityQueue.js

--- heap/dualPriorityQueue.js
+++ heap/dualPriorityQueue.js
@@ -15,27 +15,86 @@
 // 원소는 “명령어 데이터” 형식으로 주어집니다.- 최댓값/최솟값을 삭제하는 연산에서 최댓값/최솟값이 둘 이상인 경우, 하나만 삭제합니다.
 // 빈 큐에 데이터를 삭제하라는 연산이 주어질 경우, 해당 연산은 무시합니다.
 
+function operate(arr, [command, number]) {
+	if (command === 'I') arr.push(number);
+	arr.sort((a, b) => a - b);
+	if (command === 'D') {
+		if (number === 1) arr.pop();
+		if (number === -1) arr.shift();
+	}
+}
+
 function solution(operations) {
 	let operatorArr = operations.map(el => el.split(' ' ));
-	operatorArr.forEach(el => {
-		el[1] = parseInt(el[1]);
-	});
+	operatorArr.forEach(el => { el[1] = parseInt(el[1]); });
 	let arr = [];
-
-	while (operatorArr.length) {
-		let [command, number] = operatorArr.shift();
-		if (command === 'I') arr.push(number);
-		arr.sort((a, b) => a - b);
-		if (command === 'D') {
-			if (number === 1) arr.pop();
-			if (number === -1) arr.shift();
-		}
-	}
-
+	while (operatorArr.length) operate(arr, operatorArr.shift());
+	arr.sort((a, b) => a - b);
 	return arr.length ? [arr[arr.length - 1], arr[0]] : [0, 0];
 }
+
+
 
 var operations = ["I 16","D 1"];
 console.log(solution(operations)); //[0,0]
 var operations = ["I 7","I 5","I -5","D -1"];
 console.log(solution(operations)); //[7,5]
+
+
+// function getMinHeap(arr, n, root) {
+// 	let minNum = root;
+// 	let left = 2 * root + 1;
+// 	let right = 2 * root + 2;
+
+// 	if (left < n & arr[left] < arr[minNum]) minNum = left;
+// 	if (right < n & arr[right] < arr[minNum]) minNum = right;
+// 	if (minNum !== root) {
+// 		[arr[minNum], arr[root]] = [arr[root], arr[minNum]];
+// 		getMinHeap(arr, n, minNum);
+// 	}
+// }
+
+// function getMaxHeap(arr, n, root) {
+// 	let maxNum = root;
+// 	let left = 2 * root + 1;
+// 	let right = 2 * root + 2;
+
+// 	if (left < n & arr[left] > arr[maxNum]) maxNum = left;
+// 	if (right < n & arr[right] > arr[maxNum]) maxNum = right;
+// 	if (maxNum !== root) {
+// 		[arr[maxNum], arr[root]] = [arr[root], arr[maxNum]];
+// 		getMaxHeap(arr, n, maxNum);
+// 	}
+// }
+
+// function operate([minHeap, maxHeap], [command, number]) {
+// 	if (command === 'I') {
+// 		minHeap.push(number);
+// 		maxHeap.push(number);
+// 	}
+// 	getMinHeap(minHeap, minHeap.length, 0);
+// 	getMaxHeap(maxHeap, maxHeap.length, 0);
+// 	if (command === 'D') {
+// 		if (number === 1) {
+// 			minHeap.pop();
+// 			maxHeap.shift();
+// 		}
+// 		if (number === -1) {
+// 			minHeap.shift();
+// 			maxHeap.pop();
+// 		}
+// 	}
+// }
+
+// function solution(operations) {
+// 	let operatorArr = operations.map(el => el.split(' ' ));
+// 	operatorArr.forEach(el => { el[1] = parseInt(el[1]); });
+// 	let minHeap = [];
+// 	let maxHeap = [];
+// 	let arr = [minHeap, maxHeap];
+
+// 	while (operatorArr.length) operate(arr, operatorArr.shift());
+// 	getMinHeap(minHeap, minHeap.length, 0);
+// 	getMaxHeap(maxHeap, maxHeap.length, 0);
+// 	return minHeap.length ? [maxHeap.shift(), minHeap.shift()] : [0, 0];
+// }
