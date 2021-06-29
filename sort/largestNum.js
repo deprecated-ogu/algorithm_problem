26c0a6804bbbdeaffe0e4e8801a85382b7ea313f
hyoogu <idhyo0o@gmail.com>
2021-06-29 23:36:07 +0900

    solve largestNum.js

---
+++ sort/largestNum.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+	let strNumbers = numbers.map(el => String(el));
+	strNumbers.sort((a, b) => b[0] - a[0]);
+	strNumbers.sort((a, b) => (b + a) - (a + b));
+	return strNumbers[0] == 0 ? String(strNumbers.join('')) : '0';
+}
+
+var numbers = [3, 30, 34, 5, 9]
+var numbers = [40, 404]
+var numbers = [0, 0, 0, 0]
+
+console.log(solution(numbers)); // 6210
