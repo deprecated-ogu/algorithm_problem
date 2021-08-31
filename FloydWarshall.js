a14b6b073c51ad927c5d50f37ea311e57e00c7a4
hyoogu <idhyo0o@gmail.com>
2021-08-31 16:26:21 +0900

    Solve FloydWarshall.js

--- FloydWarshall.js
+++ FloydWarshall.js
@@ -16,29 +16,37 @@
 // 출력
 // 배열을 리턴해야 합니다.
 
-function createGraphByList(num, edges) {
-  const _edges = {};
-  for (let i = 1; i <= num; i++) _edges[i] = [];
-  edges.forEach(([src, dst, weight]) => {
-    _edges[src].push({ node: dst, weight: weight });
-    _edges[dst].push({ node: src, weight: weight });
-  });
-  return _edges;
-}
+const INF = 101;
 
 function createGraphByMatrix(num, edges) {
   const matrix = [];
-  const INF = 101;
   for (let i = 0; i <= num; i++) {
     matrix.push(Array(num + 1).fill(INF));
     matrix[i][i] = 0;
   }
   edges.forEach(([src, dst, weight]) => {
-    matrix[src][dst] = matrix[dst][src] = weight;
+    matrix[src][dst] = weight;
   });
   return matrix;
 }
 
 function FloydWarshall(num, edges) {
-  //
+  const dist = createGraphByMatrix(num, edges);
+  for (let stopover = 1; stopover <= num; stopover++) {
+    for (let src = 1; src <= num; src++) {
+      for (let dst = 1; dst <= num; dst++) {
+        if (dist[src][stopover] + dist[stopover][dst] < dist[src][dst]) {
+          dist[src][dst] = dist[src][stopover] + dist[stopover][dst];
+        }
+      }
+    }
+  }
+
+  const nulled = dist.map((row) =>
+    row.map((col) => {
+      if (col === INF) return null;
+      else return col;
+    })
+  );
+  return nulled.slice(1).map((row) => row.slice(1));
 }
