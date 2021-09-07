1350563f440b22638aa5bb10bec96c739f60b5db
hyoogu <idhyo0o@gmail.com>
2021-09-07 16:27:41 +0900

    Solve openChatRoom

---
+++ openChatRoom.js
@@ -0,0 +1,38 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42888
+
+function solution(record) {
+  let idToName = {};
+  let recordArr = record.map((el) => el.split(" "));
+  let resultMsg = [];
+  console.log(recordArr);
+
+  recordArr.forEach((el) => {
+    if (el[0] === "Enter") {
+      idToName[el[1]] = el[2];
+    }
+    if (el[0] === "Change") {
+      idToName[el[1]] = el[2];
+    }
+  });
+
+  recordArr.forEach((el) => {
+    if (el[0] === "Enter") {
+      resultMsg.push(`${idToName[el[1]]}님이 들어왔습니다.`);
+    }
+    if (el[0] === "Leave") {
+      resultMsg.push(`${idToName[el[1]]}님이 나갔습니다.`);
+    }
+  });
+
+  return resultMsg;
+}
+
+console.log(
+  solution([
+    "Enter uid1234 Muzi",
+    "Enter uid4567 Prodo",
+    "Leave uid1234",
+    "Enter uid1234 Prodo",
+    "Change uid4567 Ryan",
+  ])
+);
