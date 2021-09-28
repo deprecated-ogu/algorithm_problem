e7240dcd14548f3f7d5728d706f3e6fb6a0916f4
hyoogu <idhyo0o@gmail.com>
2021-09-28 09:17:58 +0900

    Refactor code

--- mathematics/nNumberGame.js
+++ mathematics/nNumberGame.js
@@ -1,17 +1,23 @@
 // https://programmers.co.kr/learn/courses/30/lessons/17687
 
-function solution(n, t, m, p) {
-  let answer = "";
+function getFullStr(n, length) {
   let numberStr = "";
   let decimal = 0;
 
-  while (numberStr.length < m * t) {
+  while (numberStr.length < length) {
     numberStr += decimal.toString(n);
     decimal++;
   }
+  return numberStr;
+}
+
+function solution(n, t, m, p) {
+  let answer = "";
+  let numberStr = getFullStr(n, m * t);
+
   for (let i = 0; i <= numberStr.length; i += m) {
     if (answer.length === t) break;
-    answer += numberStr[i + p - 1];
+    answer += numberStr[i - 1 + p];
   }
 
   return answer.toUpperCase();
@@ -22,15 +28,3 @@ var t = 10;
 var m = 2;
 var p = 1;
 console.log(solution(n, t, m, p));
-
-0;
-1;
-10;
-11;
-100;
-011011100;
-
-0;
-1;
-1;
-1;
