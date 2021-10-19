950975ce738f28558f26c39ae495c73080fc126c
hyoogu <idhyo0o@gmail.com>
2021-10-19 20:54:56 +0900

    Solve newsClustering.js

---
+++ implementation/newsClustering.js
@@ -0,0 +1,48 @@
+// https://programmers.co.kr/learn/courses/30/lessons/17677
+
+function solution(str1, str2) {
+  let tmp1 = [];
+  let tmp2 = [];
+  let inter = 0;
+
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  for (let i = 1; i < str1.length; i++) {
+    if (
+      str1[i - 1] >= "a" &&
+      str1[i - 1] <= "z" &&
+      str1[i] >= "a" &&
+      str1[i] <= "z"
+    )
+      tmp1.push(str1[i - 1] + str1[i]);
+  }
+
+  for (let i = 1; i < str2.length; i++) {
+    if (
+      str2[i - 1] >= "a" &&
+      str2[i - 1] <= "z" &&
+      str2[i] >= "a" &&
+      str2[i] <= "z"
+    )
+      tmp2.push(str2[i - 1] + str2[i]);
+  }
+
+  for (let i = 0; i < tmp1.length; i++) {
+    const tmp = tmp1[i];
+    for (let j = 0; j < tmp2.length; j++) {
+      if (tmp === tmp2[j]) {
+        inter++;
+        tmp2[j] = ".";
+        break;
+      }
+    }
+  }
+  const union = tmp1.length + tmp2.length - inter;
+
+  return union === inter ? 65536 : Math.floor((inter / union) * 65536);
+}
+
+var str1 = "aa+aa+bb+bb";
+var str2 = "AAAA+BBBB";
+console.log(solution(str1, str2));
