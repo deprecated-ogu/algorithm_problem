21b6aa10b58567a48e1a90a24202a00fc38b8741
hyoogu <idhyo0o@gmail.com>
2021-10-21 10:09:34 +0900

    Solve bestSet.js

---
+++ implementation/bestSet.js
@@ -0,0 +1,12 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12938
+
+function solution(n, s) {
+  if (n > s) return [-1];
+  const answer = new Array(n).fill(parseInt(s / n));
+  for (let i = 0; i < s % n; i++) answer[i]++;
+  return answer.sort((a, b) => a - b);
+}
+
+var n = 2;
+var s = 9;
+console.log(solution(n, s));
