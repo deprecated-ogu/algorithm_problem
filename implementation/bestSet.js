58115d285029a7d4f2db10ed2366f89e000ce371
hyoogu <idhyo0o@gmail.com>
2021-10-21 10:49:54 +0900

    Refactor bestSet.js

--- implementation/bestSet.js
+++ implementation/bestSet.js
@@ -3,8 +3,8 @@
 function solution(n, s) {
   if (n > s) return [-1];
   const answer = new Array(n).fill(parseInt(s / n));
-  for (let i = 0; i < s % n; i++) answer[i]++;
-  return answer.sort((a, b) => a - b);
+  for (let i = n - 1; i > n - 1 - (s % n); i--) answer[i]++;
+  return answer;
 }
 
 var n = 2;
